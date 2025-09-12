de1003d64986a99b21928d4bf1db7840a90df3cd
Maximus Katsoulis <maximuskatsoulis985@gmail.com>
2025-09-11 19:55:12 -0500

    Removed typeof for Custom News Check

--- customnews.js
+++ customnews.js
@@ -1,6 +1,6 @@
 /* global Game */
 
-if (typeof CustomNews == undefined) CustomNews = {};
+if (CustomNews == undefined) CustomNews = {};
 if (typeof CCSE == undefined) Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
 CustomNews.launch = function(){
     CustomNews.init = function(){
