c3c2aad9ebf8b385018adbae2181269b81856113
Maximus Katsoulis <maximuskatsoulis985@gmail.com>
2025-09-11 19:49:16 -0500

    Initial Commit

---
+++ customnews.js
@@ -0,0 +1,21 @@
+/* global Game */
+
+if (typeof CustomNews == undefined) CustomNews = {};
+if (typeof CCSE == undefined) Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
+CustomNews.launch = function(){
+    CustomNews.init = function(){
+        CustomNews.isLoaded = true;
+        Game.Notify("Custom News Loaded");
+    }
+}
+
+if(!CustomNews.isLoaded){
+    if(CCSE && CCSE.isLoaded){
+        CustomNews.launch();
+
+    }else{
+        if(!CCSE) var CCSE = {};
+        if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+        CCSE.postLoadHooks.push(CustomNews.launch)
+    }
+}
