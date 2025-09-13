da87aefba4be4ca591d1fddda6cbac8a70e29378
Maximus Katsoulis <maximuskatsoulis985@gmail.com>
2025-09-12 20:19:01 -0500

    Added the message system. Messages from the mod are shown in whatever color I define.

--- customnews.js
+++ customnews.js
@@ -5,14 +5,18 @@ if (typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/Cookie
 CustomNews.name = "CustomNews";
 CustomNews.version = "1.0.0";
 CustomNews.GameVersion = "2.052";
+
+
 CustomNews.launch = function(){
 
     CustomNews.init = function(){
         CustomNews.isLoaded = true;
         CustomNews.config = {};
+
         CCSE.customSave.push(function(){
             CCSE.config.OtherMods.CustomNews = CustomNews.config;
         });
+
         CCSE.customLoad.push(function(){
             if(CCSE.config.OtherMods.CustomNews){
                 CustomNews.config = CCSE.config.OtherMods.CustomNews;
@@ -22,12 +26,12 @@ CustomNews.launch = function(){
         });
 
         if(CustomNews.postloadHooks) {
-			for(var i = 0; i < CustomNews.postloadHooks.length; ++i) {
+			for(let i = 0; i < CustomNews.postloadHooks.length; ++i) {
 				(CustomNews.postloadHooks[i])();
 			}
 		}
-
-        var startupStr = "Custom News Loaded";
+        CustomNews.showMessage(CustomNews.config.messages[0]);
+        let startupStr = "Custom News Loaded";
         Game.Notify(startupStr, '', '', 1, 1);
     }
 
@@ -38,14 +42,42 @@ CustomNews.launch = function(){
             showCreateMessageBox: false,
             achievementsGotten: [],
             replacePercentage: 50,
+            messages: CustomNews.defaultMessages()
         };
+    }
+
+    CustomNews.addMessage = function(msg_str, hex_color ){
+        const msg = [
+            hex_color,
+            msg_str
+        ];
+        CustomNews.config.messages.push(msg);
+        return msg;
     }
 
     // Checks if the versions are correct then registers the mod
     if(CCSE.ConfirmGameVersion(CustomNews.name, CustomNews.version, CustomNews.GameVersion)){
         Game.registerMod(CustomNews.name, CustomNews);
     }
+
+    CustomNews.showMessage = function(custom_msg){
+        let newsTextEl = document.getElementById("commentsText1");
+        if (newsTextEl){
+            newsTextEl.style.color = custom_msg.hex_color;
+            newsTextEl.textContent = custom_msg.text;
+        }
+    }
+
+    CustomNews.defaultMessages = function(){
+        const defaultColor = '#852323ff';
+        return [
+            ["Maxsuulis Rocks!", defaultColor],
+            ["Hello from CustomNews mod!", defaultColor]
+        ];
+    }
 }
+
+
 if(!CustomNews.isLoaded){
     if(CCSE && CCSE.isLoaded){
         CustomNews.launch();
